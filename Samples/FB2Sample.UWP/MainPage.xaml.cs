d8564ea332e1849b5e834913b4bf1cfc9d9d4ee4
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-02-06 12:53:12 +0300

    Change UWP sample

--- Samples/FB2Sample.UWP/MainPage.xaml.cs
+++ Samples/FB2Sample.UWP/MainPage.xaml.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.Storage;
 using FB2Library;
-using System.Collections.Generic;
-using FB2Library.Elements;
-using FB2Library.Elements.Poem;
-using FB2Sample.UWP.Models;
+using FB2Library.Reader;
+using FB2Library.Reader.LineTypes;
+using Windows.UI.Text;
 
 namespace FB2Sample.UWP
 {
 	public sealed partial class MainPage : Page
 	{
 		private FB2File _file;
-		private List<BookLine> _lines;
+		private IEnumerable<IBaseLine> _lines;
 
 		public MainPage()
 		{
 			InitializeComponent();
 
 			_file = new FB2File();
-			_lines = new List<BookLine>();
 		}
 
 		private async void Choose_Click(object sender, RoutedEventArgs e)
@@ -52,160 +51,63 @@ namespace FB2Sample.UWP
 
 		private async Task OpenFileAsync(StorageFile file)
 		{
+			loading.Visibility = Visibility.Visible;
+
 			using (var s = await file.OpenStreamForReadAsync())
 			{
+				var reader = new FB2Reader();
 				try
 				{
-					using (var reader = new FB2Reader())
-					{
-						_file = await reader.LoadAsync(s);
-					}
+					_file = await reader.LoadAsync(s);
+					_lines = await reader.ReadAsync(_file);
+
+					//DisplayLines();
 				}
 				catch (Exception ex)
 				{
 					bookInfo.Text = string.Format("Error loading file : {0}", ex.Message);
 				}
+				finally
+				{
+					loading.Visibility = Visibility.Collapsed;
+					reader.Dispose();
+				}
 			}
 
-			PrepareFile();
+			
 		}
 
-		private void PrepareFile()
+		private void DisplayLines()
 		{
-			if (_file.MainBody != null)
+			foreach (var line in _lines)
 			{
-				bookInfo.Text = $"Title: {_file.MainBody.Title.TitleData[0]} {_file.MainBody.Title.TitleData[1]}";
-
-				try
+				if (line is BookHeader)
 				{
-					Task.Factory.StartNew(async () =>
+					bookContent.Children.Add(new TextBlock
 					{
-						PrepareBodies();
-
-						await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-						{
-							foreach (var line in _lines)
-							{
-								BookContent.Children.Add(line.ToView());
-							}
-						});
+						FontWeight = new FontWeight { Weight = 700 },
+						Text = ((BookHeader)line).Text
 					});
-
-					
 				}
-				catch (Exception e)
+				else if (line is BookTextLine)
 				{
-					textBlock.Text = $"Exception: {e.Message}";
+					bookContent.Children.Add(new TextBlock { Text = ((BookTextLine)line).Text });
+				}
+				else if (line is BookImage)
+				{
+					var image = ((BookImage)line);
+					bookContent.Children.Add(new Image { Width = 100, Height = 100 });
 				}
 			}
 		}
-
-		private void PrepareBodies()
-		{
-			foreach (var bodyItem in _file.Bodies)
-			{
-				AddTitle(bodyItem.Title);
-
-				foreach (SectionItem sectionItem in bodyItem.Sections)
-				{
-					PrepareTextItem(sectionItem);
-				}
-			}
-		}
-
-		private void PrepareTextItems(IEnumerable<IFb2TextItem> items)
-		{
-			foreach (var item in items)
-			{
-				if (item is IFb2TextItem)
-				{
-					PrepareTextItem(item);
-				}
-				else
-				{
-					_lines.Add(new BookTextLine(item.ToString()));
-				}
-			}
-		}
-
-		private void PrepareTextItem(IFb2TextItem textItem)
-		{
-			if (textItem is CiteItem)
-			{
-				PrepareTextItems(((CiteItem)textItem).CiteData);
-				return;
-			}
-
-			if (textItem is PoemItem)
-			{
-				var item = (PoemItem)textItem;
-				AddTitle(item.Title);
-				PrepareTextItems(item.Content);
-				return;
-			}
-
-			if (textItem is SectionItem)
-			{
-				var item = (SectionItem)textItem;
-				AddTitle(item.Title);
-				PrepareTextItems(item.Content);
-				return;
-			}
-
-			if (textItem is StanzaItem)
-			{
-				var item = (StanzaItem)textItem;
-				AddTitle(item.Title);
-				PrepareTextItems(item.Lines);
-				return;
-			}
-
-			if (textItem is ParagraphItem
-				|| textItem is EmptyLineItem)
-			{
-				_lines.Add(new BookTextLine(textItem.ToString()));
-				return;
-			}
-
-			if (textItem is ImageItem)
-			{
-				var item = (ImageItem)textItem;
-				var key = item.HRef.Replace("#", string.Empty);
-
-				if (_file.Images.ContainsKey(key))
-				{
-					var data = _file.Images[key].BinaryData;
-					_lines.Add(new BookImage(data));
-				}
-				return;
-			}
-
-			throw new Exception(textItem.GetType().ToString());
-		}
-
-		
-
-	
-
-		private void AddTitle(TitleItem titleItem)
-		{
-			if (titleItem != null)
-			{
-				foreach (var title in titleItem.TitleData)
-				{
-					_lines.Add(new BookHeader(title.ToString()));
-				}
-			}
-		}
-
 
 		private void Close_Click(object sender, RoutedEventArgs e)
 		{
 			bookInfo.Text = string.Empty;
-			_file = null;
 			textBlock.Text = "Closed";
-			_lines.Clear();
-			BookContent.Children.Clear();
+			_file = null;
+			_lines = null;
+			bookContent.Children.Clear();
 		}
 	}
 }
